535b39cf63fdcf67f99db8fc54db2832b79abcbe
Kmin <alsry4547@gmail.com>
2023-10-17 23:27:13 +0900

    first Commit -> 환경설정중

---
+++ bookManagementServer/src/main/java/com/mink/bookManagementServer/App.java
@@ -0,0 +1,13 @@
+package com.mink.bookManagementServer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
