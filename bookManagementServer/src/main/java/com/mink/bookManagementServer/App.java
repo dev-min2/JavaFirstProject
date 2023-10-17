db496ae79514f7cf57eef46784ac005442693799
Kmin <alsry4547@gmail.com>
2023-10-18 02:12:07 +0900

    환경설정중

--- bookManagementServer/src/main/java/com/mink/bookManagementServer/App.java
+++ bookManagementServer/src/main/java/com/mink/bookManagementServer/App.java
@@ -1,13 +1,28 @@
 package com.mink.bookManagementServer;
 
+import SockNet.NetServer;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        NetServer s = new NetServer(9999);
+        
+        try {
+        	s.startServer();
+        }
+        catch(Exception e) {
+        	e.printStackTrace();
+        }
+        
+        while(true) {
+        	Thread.currentThread().sleep(10000000);
+        }
+        
+        
     }
 }
---
+++ bookManagementServer/src/main/java/com/mink/bookManagementServer/TestA.java
@@ -0,0 +1,10 @@
+package com.mink.bookManagementServer;
+
+public class TestA {
+
+	private int a = 10;
+	
+	public TestA() {
+		this.a = 50;
+	}
+}
